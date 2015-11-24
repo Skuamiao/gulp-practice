20fbbc2d6ccd28f719f934d09de0e2136cfea82c
Jayden Hu(胡竟荣) <CI10048@careerintlinc.local>
2015-11-24 15:04:07 +0800

    加入 gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
-    gulp_less = require('gulp-less');
+    gulp_less = require('gulp-less'),
+    watch = require('gulp-watch');
 
-gulp.task('demo', function () {
-  gulp.src('./assets/styles/**/demo.less')
-    .pipe(gulp_less({
-       paths: ['./assets']
-    }))
-    .pipe(gulp.dest('./assets/styles'));
+gulp.task('one', function () {
+    return gulp.src('./assets/styles/one.less')
+                .pipe(watch('./assets/styles/one.less'))
+                .pipe(gulp_less({
+                    paths: ['./assets/styles/']
+                }))
+                .pipe(gulp.dest('./assets/styles'));
 });
